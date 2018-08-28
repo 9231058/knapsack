48092e4b5623d5973612c4ce89c441a678e824e3
Parham Alvani <parham.alvani@gmail.com>
2018-08-28 23:12:37 +0430

    Completes problem formulation.

--- problem/problem.go
+++ problem/problem.go
@@ -13,6 +13,8 @@
 
 package problem
 
+import "math"
+
 // Item is an entity with weight and cost that store
 // in knapsack.
 type Item struct {
@@ -22,6 +24,8 @@ type Item struct {
 
 // Problem is a knapsack problem instance
 // with gradian and objective function.
+// we formulate it as a minimization problem
+// with logarithmic barrier and penalty methods.
 type Problem struct {
 	Capacity float64 // knapsack capacity
 	Items    []Item  // items that can be stored in knapsack
@@ -33,27 +37,33 @@ type Problem struct {
 // must not modify x.
 func (p Problem) Func(x []float64) float64 {
 	var sum float64
+	var penalty float64
+	var barrier float64
 
 	for i, item := range p.Items {
-		sum += item.Cost * x[i]
-		sum -= p.Mu * (item.Weight * x[i])
-		sum += p.Mu * (x[i]*x[i] - x[i])
-		sum += p.Mu * (1 - x[i])
+		sum -= item.Cost * x[i]
+		barrier -= item.Weight * x[i]
+		penalty += (x[i]*x[i] - x[i]) * (x[i]*x[i] - x[i])
 	}
-	sum += p.Mu * p.Capacity
+	barrier += p.Capacity
 
-	return sum
+	return sum + (1/p.Mu)*penalty - (p.Mu)*math.Log(barrier)
 }
 
 // Grad evaluates the gradient at x and stores the result in-place in grad.
 // Grad must not modify x.
 func (p Problem) Grad(grad []float64, x []float64) {
+	var weights float64
+
+	for i, item := range p.Items {
+		weights -= item.Weight * x[i]
+	}
+	weights += p.Capacity
 
 	for i, item := range p.Items {
 		grad[i] = 0
-		grad[i] += item.Cost
-		grad[i] -= p.Mu * item.Weight
-		grad[i] += p.Mu * (2*x[i] - 1)
-		grad[i] += p.Mu * (-1)
+		grad[i] -= item.Cost
+		grad[i] += p.Mu * item.Weight * (1 / weights)
+		grad[i] += (1 / p.Mu) * 2 * (x[i]*x[i] - x[i]) * (2*x[i] - 1)
 	}
 }
